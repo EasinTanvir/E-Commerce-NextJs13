8012bd4ad4dbbd0d0c98df3786ddaf298ed21e60
EasinTanvir <easintanvir102@gmail.com>
2024-01-08 11:01:07 +0600

    update search functionality

--- src/components/Navbar/SearchBar.jsx
+++ src/components/Navbar/SearchBar.jsx
@@ -1,47 +1,32 @@
 "use client";
-import qs from "query-string";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
+import { useState } from "react";
 
 const SearchBar = () => {
-  const router = useRouter();
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm({ defaultValues: { searchTerm: "" } });
-  console.log(errors);
+  const [data, setData] = useState("");
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const params = new URLSearchParams(searchParams);
 
-  const onSearchHandler = (data) => {
-    console.log(data);
-    if (!data.searchTerm) {
-      router.push("/");
-    }
-    const url = qs.stringifyUrl(
-      {
-        url: "/",
-        query: {
-          searchTerm: data.searchTerm,
-        },
-      },
-      { skipNull: true }
-    );
-    if (url) {
-      router.push(url);
-      reset();
-    }
+  const router = useRouter();
+
+  const onSearchHandler = () => {
+    if (data === "") return;
+
+    params.set("searchTerm", data);
+    router.push(`${pathname}?${params}`);
   };
   return (
     <div className="flex justify-center items-center md:static absolute top-16 left-0 w-full">
       <input
-        {...register("searchTerm")}
+        onChange={(e) => setData(e.target.value)}
+        required
         placeholder="Search here"
         className="lg:w-96 md:w-72 w-full  px-4 md:py-2 py-3 md:rounded-l-md rounded-none outline-none focus:md:border-[0.5px] border-gray-400"
         type="text"
       />
       <button
-        onClick={handleSubmit(onSearchHandler)}
+        onClick={onSearchHandler}
         className="bg-teal-500 text-white md:rounded-r-md rounded-none  px-4 md:py-2 py-3"
       >
         Search
